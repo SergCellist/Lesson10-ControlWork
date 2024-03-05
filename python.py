f1d0176d6b04fcdd6d01c400e65d32a6da8949e4
SergeyCellist1991 <serg311091@gmail.com>
2024-03-05 20:36:53 +0300

    Second

--- python.py
+++ python.py
@@ -7,3 +7,11 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 print(data)
+
+# One Hot
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
